21b40cc1cf1af8713ec1da525906fe0a897a87af
maheboobkelp <151819481+maheboobkelp@users.noreply.github.com>
2024-01-08 12:32:10 +0530

    Update routes.test.js

--- web/tests/routes.test.js
+++ web/tests/routes.test.js
@@ -15,7 +15,7 @@ describe('New Endpoint', () => {
       .get('/ice-flakes')
     expect(res.body).toEqual({
       resource: 'ice-flakes',
-      count: 1005,
+      count: 205,
       shape: 'rectangle'
     })
     expect(res.statusCode).toEqual(201)
